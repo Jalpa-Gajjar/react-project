4486453ae6185f603fa4a1601f4c29ec97583612
Jalpa Vadgama <jalpavadgama52@gmail.com>
2025-01-24 15:50:17 +0530

    vite react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,15 @@
 import Test from "./Title"
 
 function App() {
-
+ const username = "Welcome"
   return (
     <>
-    <h1>Hello Vite React</h1>
+    <h1>Hello Vite React {username}</h1>  
     <Test/>
     </>
   )
 }
 
 export default  App
+
+// here username is Evaluated expression (final outcome)
